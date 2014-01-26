0a4d6881bb7df5b0d3e99030681859d2f186876d
spencerbug <spencer.neilan@gmail.com>
2014-01-26 17:50:08 -0600

    InitialCommit

---
+++ Firmware/default/TinyQED.d
@@ -0,0 +1,52 @@
+TinyQED.d TinyQED.o TinyQED.o: .././TinyQED.c \
+  x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/stdlib.h \
+  x:\program\ files\ (x86)\arduino\hardware\tools\avr\bin\../lib/gcc/avr/4.3.2/include/stddef.h \
+  x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/io.h \
+  x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/inttypes.h \
+  x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/stdint.h \
+  x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/iotn45.h \
+  x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/iotnx5.h \
+  x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/portpins.h \
+  x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/common.h \
+  x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/version.h \
+  x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/fuse.h \
+  x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/lock.h \
+  x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/interrupt.h \
+  x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/eeprom.h \
+  .././usiTwiSlave.h \
+  x:\program\ files\ (x86)\arduino\hardware\tools\avr\bin\../lib/gcc/avr/4.3.2/include/stdbool.h
+
+x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/stdlib.h:
+
+x:\program\ files\ (x86)\arduino\hardware\tools\avr\bin\../lib/gcc/avr/4.3.2/include/stddef.h:
+
+x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/io.h:
+
+x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/inttypes.h:
+
+x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/stdint.h:
+
+x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/iotn45.h:
+
+x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/iotnx5.h:
+
+x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/portpins.h:
+
+x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/common.h:
+
+x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/version.h:
+
+x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/fuse.h:
+
+x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/lock.h:
+
+x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/interrupt.h:
+
+x:/program\ files\ (x86)/arduino/hardware/tools/avr/lib/gcc/../../avr/include/avr/eeprom.h:
+
+.././usiTwiSlave.h:
+
+x:\program\ files\ (x86)\arduino\hardware\tools\avr\bin\../lib/gcc/avr/4.3.2/include/stdbool.h:
